6e28649b4f0b6720952e40ed18f26325531724fb
Ronald Arias <ariasron@hotmail.com>
2022-04-23 15:09:50 -0500

    adds a signleTodo component and a TodoList component, styles still in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
+import TodoList from './components/TodoList';
 import { Todo } from './model';
 
 const App: React.FC = () => {
@@ -26,8 +27,7 @@ const App: React.FC = () => {
     <div className="App">
       <span className="heading">Taskify</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      {/* <TodoList /> */}
-      {todos.map(t => <li key={t.id}>{t.todo}</li>)}
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 }
