33e9d7a57cd3834ec59aa43eabebe29a30405a0f
Ronald Arias <ariasron@hotmail.com>
2022-04-24 14:08:26 -0500

    done functionality is working

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -11,13 +11,22 @@ type Props = {
 }
 
 const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+
+  const handleDone = (id: number) => {
+    setTodos(todos.map((todo) => todo.id === id ? {...todo, isDone: !todo.isDone} : todo))
+  }
+
   return (
     <form className="todos__single">
-      <span className="todos__single--text">{todo.todo}</span>
+      {todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
       <div>
         <span className="icon"><AiFillEdit/></span>
         <span className="icon"><AiFillDelete/></span>
-        <span className="icon"><MdDone/></span>
+        <span className="icon" onClick={() => handleDone(todo.id)}><MdDone/></span>
       </div>
     </form>
   )
