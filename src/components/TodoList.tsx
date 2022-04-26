2c7db9cd9d2637e416b267d6e29cecda0731a1c9
Ronald Arias <ariasron@hotmail.com>
2022-04-26 15:55:33 -0500

    builds UI for drag and drop completed tasks

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -10,10 +10,23 @@ interface Props {
 
 const TodoList:React.FC<Props> = ({ todos, setTodos }) => {
   return (
-    <div className='todos'>{
-      todos.map((todo) => (
-        <SingleTodo key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />
-      ))}
+    <div className='container'>
+      <div className="todos">
+        <span className="todos__heading">
+          Active Tasks
+        </span>
+        {todos.map((todo) => (
+          <SingleTodo todo={todo} todos={todos} setTodos={setTodos} key={todo.id}/>
+        ))}
+        </div>
+        <div className="todos remove">
+        <span className="todos__heading">
+          Completed Tasks
+        </span>
+        {todos.map((todo) => (
+          <SingleTodo todo={todo} todos={todos} setTodos={setTodos} key={todo.id}/>
+        ))}
+        </div>
     </div>
   )
 }
